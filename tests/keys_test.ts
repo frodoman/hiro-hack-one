b261a517d9d941cc0a2efb2471201da2ffde604e
xinghou.liu <x_coder_20090203@hotmail.co.uk>
2023-12-06 09:32:33 +0000

    Add unit tests for setting/getting protocol fees.

--- tests/keys_test.ts
+++ tests/keys_test.ts
@@ -12,7 +12,6 @@ Clarinet.test({
         chain.mineBlock([
         ]);
 
-        
         const funCall = chain.callReadOnlyFn(
             'keys',
             'is-keyholder',
@@ -89,12 +88,27 @@ Clarinet.test({
 });
 
 Clarinet.test({
-    name: "keys-005: Ensure that balance is updated after buying a key.",
+    name: "keys-005: Ensure that amount is bigger than 0 for buying keys",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        // arrange: set up the chain, state, and other required elements
+        let deployer = accounts.get("deployer")!.address;
+
+        // act: perform actions related to the current test
+        let block = chain.mineBlock([
+           Tx.contractCall('keys', 'buy-keys', [types.principal(deployer), types.uint(0)], deployer)
+        ]);
+
+        const result = block.receipts[0].result;
+        result.expectErr().expectUint(0);
+    },
+});
+
+Clarinet.test({
+    name: "keys-006: Ensure that balance is updated after buying a key.",
     async fn(chain: Chain, accounts: Map<string, Account>) {
         // deployer address
         let deployer = accounts.get("deployer")!.address;
 
-        //balance should be u0 before buying
         chain.mineEmptyBlockUntil(1)
 
         // call get-price function
@@ -104,7 +118,7 @@ Clarinet.test({
             [types.principal(deployer), types.principal(deployer)],
             deployer
         );
-
+        //balance should be u0 before buying
         funCallBefore.result.expectUint(0)
 
         // buy some keys 
@@ -123,3 +137,105 @@ Clarinet.test({
     },
 });
 
+Clarinet.test({
+    name: "keys-007: Ensure that kyes supply is updated after buying.",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        // deployer address
+        let deployer = accounts.get("deployer")!.address;
+
+        chain.mineEmptyBlockUntil(1)
+
+        // call get-price function
+        const funCallBefore = chain.callReadOnlyFn(
+            'keys',
+            'get-keys-supply',
+            [types.principal(deployer)],
+            deployer
+        );
+        //balance should be u0 before buying
+        funCallBefore.result.expectUint(0)
+
+        // buy some keys 
+        chain.mineBlock([
+           Tx.contractCall('keys', 'buy-keys', [types.principal(deployer), types.uint(200)], deployer)
+        ]);
+
+        // call get-price function
+        const funCallAfter = chain.callReadOnlyFn(
+            'keys',
+            'get-keys-supply',
+            [types.principal(deployer)],
+            deployer
+        );
+        funCallAfter.result.expectUint(200)
+    },
+});
+
+Clarinet.test({
+    name: "keys-008: Ensure that protocol fee can be updated.",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        // deployer address
+        let deployer = accounts.get("deployer")!.address;
+        const readFunName = 'get-protocol-fee-percent'
+
+        chain.mineEmptyBlockUntil(1)
+
+        // fee should be u200 by default
+        const funCallBefore = chain.callReadOnlyFn(
+            'keys',
+            readFunName,
+            [],
+            deployer
+        );
+        funCallBefore.result.expectUint(200)
+
+        // update the fee
+        chain.mineBlock([
+           Tx.contractCall('keys', 'set-protocol-fee-percent', [types.uint(500)], deployer)
+        ]);
+
+        // call get-price function
+        const funCallAfter = chain.callReadOnlyFn(
+            'keys',
+            readFunName,
+            [],
+            deployer
+        );
+        funCallAfter.result.expectUint(500)
+    },
+});
+
+Clarinet.test({
+    name: "keys-009: Ensure that only the deployer can update protocol fee.",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        // deployer address
+        let wallet_1 = accounts.get("wallet_1")!.address;
+
+        // update the fee
+        const block = chain.mineBlock([
+           Tx.contractCall('keys', 'set-protocol-fee-percent', [types.uint(500)], wallet_1)
+        ]);
+
+        // call get-price function
+        const result = block.receipts[0].result;
+        result.expectErr().expectUint(4)
+    },
+});
+
+
+Clarinet.test({
+    name: "keys-010: Ensure that protocol fee can not be set to u0.",
+    async fn(chain: Chain, accounts: Map<string, Account>) {
+        // deployer address
+        let deployer = accounts.get("deployer")!.address;
+
+        // update the fee
+        const block = chain.mineBlock([
+           Tx.contractCall('keys', 'set-protocol-fee-percent', [types.uint(0)], deployer)
+        ]);
+
+        // call get-price function
+        const result = block.receipts[0].result;
+        result.expectErr().expectUint(5)
+    },
+});
